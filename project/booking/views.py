d4657237fbdf317048c359d54362446bb70dd5e0
davidromachshenko <dav.1997@mail.ru>
2021-09-17 12:34:14 +0600

    fix checking date

--- project/booking/views.py
+++ project/booking/views.py
@@ -1,12 +1,13 @@
 import os
 
+from datetime import datetime
 from django.core.cache import cache
 from django.http import JsonResponse
 
 
 def get_ticket(request):
     try:
-        date = request.GET["date"].strftime(os.environ.get("DATE_FORMAT"))
+        date = datetime.strptime(request.GET["date"], os.environ.get("DATE_FORMAT"))
         fly_from = request.GET["fly_from"]
         fly_to = request.GET["fly_to"]
     except KeyError as e:
